8767ea296d32a80dc229396651d39dc9348a953a
yekyawaung91 <yekyawaung1991@gmail.com>
2025-09-28 20:08:12 +0630

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { TonConnectButton } from '@tonconnect/ui-react';
+
+function App() {
+  return (
+    <>
+    <TonConnectUIProvider manifestUrl="http://localhost:5173/tonconnect-manifest.json">
+      <header>
+      <span>My App with React UI</span>
+        <TonConnectButton />
+      </header>
+    </TonConnectUIProvider>
+      
+    </>
+  )
+}
+
+export default App
